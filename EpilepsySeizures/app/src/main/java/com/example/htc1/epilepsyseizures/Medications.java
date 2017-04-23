a1ee9cc3a535a39763692f33bd511f2054f5771e
javanotepad <ahmedalmahlasi@gmail.com>
2017-04-23 11:25:06 +0300

    second project

---
+++ EpilepsySeizures/app/src/main/java/com/example/htc1/epilepsyseizures/Medications.java
@@ -0,0 +1,13 @@
+package com.example.htc1.epilepsyseizures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Medications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medications);
+    }
+}
